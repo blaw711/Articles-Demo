c9eddb764745abcddf57cbbc7304ed062a98c09a
blaw711 <blaw711@comcast.net>
2017-09-16 19:02:34 -0400

    Initial Commit

---
+++ Articles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Articles
+//
+//  Created by Bob Law on 9/16/17.
+//  Copyright © 2017 Bob Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
